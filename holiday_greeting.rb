d36707d2a510146a64c3556379908b911cbecc0d
meredithheller <meredithheller@outlook.com>
2018-08-07 16:29:00 +0000

    automatically backed up by learn

---
+++ holiday_greeting.rb
@@ -0,0 +1,7 @@
+puts "Hi, create your custom holiday card here. "
+
+def holiday_card(to = "Mom",from = "Your Favorite Child", holiday = "Mother's Day")
+  "Happy #{holiday}, #{to}! From #{from}"
+end
+
+puts holiday_card()
